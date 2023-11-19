7761cd995255ff5092b6413053667641e5cd0021
Diska <115615910+Diska404@users.noreply.github.com>
2023-11-19 19:50:47 +0700

    Lab7

---
+++ lab7_php_dasar/php_dasar.php/php_dasar.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+       <title>Belajar PHP</title> 
+    </head>
+
+    <body>
+        <h1>Belajar PHP Dasar</h1>
+
+        <?php 
+        echo"Helllo word"
+        ?>
+        <h3>Menggunkan variable</h3>
+
+        <?php 
+        $nim = "312210369";
+        $nama = 'Diska Kurnia Azzahra Putra';
+        echo "NIM : " . $nim . "<br>";
+        echo "Nama : $nama";
+        ?>
+    </body>
+</html>
